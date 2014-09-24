706db23e860bf8a99f52b9eb9e7df09b58209098
despertargz <mev412@gmail.com>
2014-09-24 00:04:37 -0500

    added missing project

---
+++ Mev.Events.Lib/Class1.cs
@@ -0,0 +1,84 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mev.Events.Lib
+{
+    public class Event
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public DateTime? Completed { get; set; }
+        public DateTime? Due { get; set; }
+
+        public string Subject { get; set; }
+        public string Description { get; set; }
+
+        public Priority Priority { get; set; }
+        public Status Status { get; set; }
+
+        public List<Comment> Comments { get; set; }
+        public List<Alarm> Alarms { get; set; }
+        public List<string> Tags { get; set; }
+
+        public dynamic Data { get; set; }
+
+        public Event()
+	    {
+            this.Comments = new List<Comment>();
+            this.Alarms = new List<Alarm>();
+            this.Tags = new List<string>();
+            this.Data = new ExpandoObject();
+	    }
+
+        // children
+        // order
+        // group
+    }
+
+    public class Tag
+    {
+        public string Name { get; set; }
+    }
+
+    public class Alarm
+    {
+        public string Message { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime FireAt { get; set; }
+    }
+
+    public class Comment
+    {
+        public string Message { get; set; }
+        public DateTime Time { get; set; }
+    }
+
+    public enum Status
+    {
+        Pending = 0,
+        Working = 1,
+        Completed = 2,
+        Waiting = 3,
+        Blocked = 4,
+        Cancelled = 5
+    }
+
+    public enum Priority
+    {
+        VeryLow = 1,
+        Low = 2,
+        Medium = 3,
+        High = 4,
+        VeryHigh = 5
+    }
+}
---
+++ Mev.Events.Lib/Program.cs
@@ -0,0 +1,58 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mev.Events.Lib
+{
+    class Program
+    {
+        static void Main()
+        {
+            MongoClient client = new MongoClient("mongodb://localhost");
+            var server = client.GetServer();
+            var database = server.GetDatabase("testdb");
+            var collection = database.GetCollection("stuff");
+
+
+
+
+            var events = collection.FindAll();
+            
+
+            //Event myEvent = new Event()
+            //{
+            //    Created = DateTime.Now,
+            //    Description = "this is a description",
+            //    Priority = Priority.VeryHigh,
+            //    Status = Status.Working,
+            //    Subject = "this is a subject",
+            //    Updated = DateTime.Now.AddSeconds(5),
+            //    Id = 3
+            //};
+
+            //myEvent.Comments = new List<Comment>();
+            //myEvent.Comments.Add(new Comment() { Message = "hey!", Time = DateTime.Now });
+
+            //myEvent.Data = new ExpandoObject();
+            //myEvent.Data.place = "steak n shake";
+            //myEvent.Data.cost = "$100";
+
+            //var one = collection.FindOneByIdAs<Event>(1);
+            //one.Subject = "One";
+            
+
+            //collection.FindOneByIdAs<Event>(2).Subject = "Two";
+            //collection.FindOneByIdAs<Event>(3).Subject = "Three";
+
+            Console.WriteLine("!");
+            Console.ReadLine();
+
+        }
+    }
+}
